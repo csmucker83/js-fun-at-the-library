7ca004b25350fc8b4d1866f8d1c757114995d0e3
Christian Smucker <chrissmucker94@gmail.com>
2021-07-08 17:58:13 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+}
+
+function saveReview(review, reviews) {
+  if (reviews.includes(review)) {
+    return reviews;
+  } else {
+    return reviews.push(review);
+  }
+};
+
+function calculatePageCount(bookPageCount) {
+  return bookPageCount.length * 20;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: title.length * 20,
+    genre: "fantasy",
+  }
+}
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
